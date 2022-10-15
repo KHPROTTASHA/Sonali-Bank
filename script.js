d10a709d653bb30a4680103fafcb17af9cc4b363
KHPROTTASHA <prottashakhp@gmail.com>
2022-10-16 00:00:58 +0530

    welcome

--- script.js
+++ script.js
@@ -6,56 +6,48 @@ submitBtn.addEventListener("click", function(){
     transactionArea.style.display = "block";
 })
 //deposit area....
-const depositButt = document.getElementById("deposit-butt");
-      depositButt.addEventListener("click",function(){
-       
+    const depositButt = document.getElementById("deposit-butt");
+    depositButt.addEventListener("click",function(){
     const depositAmount = document.getElementById("deposit-amount").value;
     const depositNumber = parseFloat(depositAmount);
-    
-
-     const currentDeposit = document.getElementById("current-deposit").innerText;
-    const currentDepositNumbr = parseFloat(currentDeposit);
-    const totalDeposit = depositNumber + currentDepositNumbr;
-    document.getElementById("current-deposit").innerText = totalDeposit
-     
-     const cBalance = document.getElementById("c-balance").innerText;
-     const currentBalance = parseFloat(cBalance);
-     const totalBalance = depositNumber + currentBalance;
-     document.getElementById("c-balance").innerText = totalBalance;
-    // taka ("c-balance",depositNumber );
-    // taka ("current-deposit",depositNumber );
-
-
+    //  const currentDeposit = document.getElementById("current-deposit").innerText;
+    // const currentDepositNumbr = parseFloat(currentDeposit);
+    // const totalDeposit = depositNumber + currentDepositNumbr;
+    // document.getElementById("current-deposit").innerText = totalDeposit
+    taka ("current-deposit",depositNumber)
+    //  const cBalance = document.getElementById("c-balance").innerText;
+    //  const currentBalance = parseFloat(cBalance);
+    //  const totalBalance = depositNumber + currentBalance;
+    //  document.getElementById("c-balance").innerText = totalBalance;
+    taka ("c-balance",depositNumber)
     document.getElementById("deposit-amount").value ="";
 })
-
-// function taka(id ,depositNumber) {
-//     const cBalance = document.getElementById(id).innerText;
-//     const currentBalance = parseFloat (cBalance);
-//     const totalBalance = depositNumber + currentBalance;
-//     document.getElementById("c-balance").innerText = totalBalance;
-// }
-
+function taka(id, depositNumber) {
+ const Balance = document.getElementById(id).innerText;
+ const currentB = parseFloat(Balance);
+ const total = depositNumber + currentB;
+ document.getElementById(id).innerText = total;
+}
 //withdraw..
-     const withdrawButt = document.getElementById("withdraw-butt");
-     withdrawButt.addEventListener("click",function(){
-       
+    const withdrawButt = document.getElementById("withdraw-butt");
+    withdrawButt.addEventListener("click",function(){
     const withdrawAmount = document.getElementById("withdraw-amount").value;
     const withdrawNumber = parseFloat(withdrawAmount);
-    
-
-     const currentwithdraw = document.getElementById("current-withdraw").innerText;
-    const currentwithdrawNumbr = parseFloat(currentwithdraw);
-    const totalwithdraw = withdrawNumber + currentwithdrawNumbr;
-    document.getElementById("current-withdraw").innerText = totalwithdraw
-     
-     const cBalance = document.getElementById("c-balance").innerText;
-     const currentBalance = parseFloat(cBalance);
-     const totalBalance = -1*withdrawNumber + currentBalance;
-     document.getElementById("c-balance").innerText = totalBalance;
-    
-
-
-    document.getElementById("deposit-amount").value ="";
+    //  const currentwithdraw = document.getElementById("current-withdraw").innerText;
+    // const currentwithdrawNumbr = parseFloat(currentwithdraw);
+    // const totalwithdraw = withdrawNumber + currentwithdrawNumbr;
+    // document.getElementById("current-withdraw").innerText = totalwithdraw
+    taka("current-withdraw",withdrawNumber)
+    //  const cBalance = document.getElementById("c-balance").innerText;
+    //  const currentBalance = parseFloat(cBalance);
+    //  const totalBalance = -1*withdrawNumber + currentBalance;
+    //  document.getElementById("c-balance").innerText = totalBalance;
+    taka("c-balance",-1*withdrawNumber)
     document.getElementById("withdraw-amount").value="";
 })
+function taka(id,withdrawNumber) {
+const currentwithdraw = document.getElementById(id).innerText;
+const currentwithdrawNumbr = parseFloat(currentwithdraw);
+const totalwithdraw = withdrawNumber + currentwithdrawNumbr;
+document.getElementById(id).innerText = totalwithdraw
+}
